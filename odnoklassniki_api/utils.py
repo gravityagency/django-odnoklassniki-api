d20be315511704d0164b7cbd8181e5f027510049
ramusus <ramusus@gmail.com>
2014-04-09 20:44:25 +0600

    wrote ok_groups using this release

--- odnoklassniki_api/utils.py
+++ odnoklassniki_api/utils.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from oauth_tokens.models import AccessToken, AccessTokenGettingError
 from ssl import SSLError
-from odnoklassniki import api
+from odnoklassniki import api, OdnoklassnikiError
 import time
 import logging
 
@@ -40,7 +40,8 @@ def get_api(used_access_tokens=None, *args, **kwargs):
     if ACCESS_TOKEN:
         token = ACCESS_TOKEN
     else:
-        tokens = AccessToken.objects.filter_active_tokens_of_provider('odnoklassniki', *args, **kwargs)
+#        tokens = AccessToken.objects.filter_active_tokens_of_provider('odnoklassniki', *args, **kwargs)
+        tokens = AccessToken.objects.filter(provider='odnoklassniki', *args, **kwargs).order_by('-id')
         if used_access_tokens:
             tokens = tokens.exclude(access_token__in=used_access_tokens)
 
@@ -49,31 +50,31 @@ def get_api(used_access_tokens=None, *args, **kwargs):
         else:
             token = tokens[0].access_token
 
-    return api.Odnoklassniki(application_key=APPLICATION_PUBLIC, application_secret=APPLICATION_PUBLIC, token=token)
+    return api.Odnoklassniki(application_key=APPLICATION_PUBLIC, application_secret=APPLICATION_SECRET, token=token)
 
 def api_call(method, recursion_count=0, methods_access_tag=None, used_access_tokens=None, **kwargs):
     '''
     Call API using access_token
     '''
     try:
-        api_instance = get_api(tag=methods_access_tag, used_access_tokens=used_access_tokens)
+        api = get_api(tag=methods_access_tag, used_access_tokens=used_access_tokens)
     except NoActiveTokens, e:
         if used_access_tokens:
             # we should wait 1 sec and repeat with clear attribute used_access_tokens
             log.warning("Waiting 1 sec, because all active tokens are used, method: %s, recursion count: %d" % (method, recursion_count))
             time.sleep(1)
-            return api_instance(method, recursion_count+1, methods_access_tag, used_access_tokens=None, **kwargs)
+            return api_call(method, recursion_count+1, methods_access_tag, used_access_tokens=None, **kwargs)
         else:
             log.warning("Suddenly updating tokens, because no active access tokens and used_access_tokens empty, method: %s, recursion count: %d" % (method, recursion_count))
             update_tokens()
-            return api_instance(method, recursion_count+1, methods_access_tag, **kwargs)
+            return api_call(method, recursion_count+1, methods_access_tag, **kwargs)
 
     try:
-        response = api_instance._get(method, timeout=TIMEOUT, **kwargs)
+        response = api._get(method, **kwargs)
     except SSLError, e:
         log.error("SSLError: '%s' registered while executing method %s with params %s, recursion count: %d" % (e, method, kwargs, recursion_count))
         time.sleep(1)
-        return api_instance(method, recursion_count+1, methods_access_tag, **kwargs)
+        return api_call(method, recursion_count+1, methods_access_tag, **kwargs)
     except Exception, e:
         log.error("Unhandled error: %s registered while executing method %s with params %s" % (e, method, kwargs))
         raise e
